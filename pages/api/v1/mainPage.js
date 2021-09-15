96eb24c1f8de6cae416808c4c575866d408c3366
unknown <maikonweber@gmail.com>
2021-09-14 23:07:20 -0300

    Adding Posgreets

--- pages/api/v1/mainPage.js
+++ pages/api/v1/mainPage.js
@@ -1,5 +1,35 @@
-export default function handler(req, res) {
-        
-    res.status(200).json({ name: 'John Doe' })
+const pgp = require('pg-promise')({
+    noWarnings: true
+})
+
+const user = 'gdwsmbxvlcojau'
+const pass = '63c2e09bda8f19a4d1e606b783f63931545e886779ec5479cd5a8769e04367c5'
+const host = 'ec2-3-219-111-26.compute-1.amazonaws.com'
+const data = 'd3d3l0dic9psnm'
+const port = '5432'
+
+
+const db = pgp(`postgres//${user}:${pass}@${host}:5432/${data}`)
+
+export default async function handler(req, res) {
+
+    try {
+        const { name, price,  imageUrl, description } = req.query
+
+        if(!name || !price || !imageUrl || !description){
+            return res.status(422).send({error: ['Missing one or more fields']})
+        }
+
+        const product = await db.one('INSERT INTO products(name, price, imageUrl, description) VALUES($1, $2, $3, $4) RETURNING *', [name, price, imageUrl, description])
+
+        res.status(200).json(product)
+
+    } catch (error) {
+        // console.error(error);
+        res.status(500).send({message: ["Error creating on the server"], error: error})
+    }
 }
+        
+  
+
   
